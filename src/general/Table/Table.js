c79e421bb186ddd95d0a1a40e87ce6af29a55bbc
thuhien191197 <thuhien191197@gmail.com>
2018-12-02 00:06:16 +0700

    edit taisan

--- src/general/Table/Table.js
+++ src/general/Table/Table.js
@@ -10,12 +10,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import { Checkbox } from '@material-ui/core';
+import { Checkbox, Icon } from '@material-ui/core';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
 import Tooltip from '@material-ui/core/Tooltip';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
@@ -276,17 +277,22 @@ class Table1 extends Component {
 													funcs.map((func, i) => {
 														console.log("func:",func)
 															return(
-																// {item == "edit"?'':''}
-																<Button 
-																	className={classes.btnEdit}
-																	variant="fab" 
-																	color="primary" 
-																	aria-label="Add"
-																	component={App}
-																>
-																	
-																	<AddIcon  />
-																</Button>
+																func === "edit"
+																?
+																	<Button 
+																		className={classes.btnEdit}
+																		variant="fab" 
+																		// color="secondary" 
+																		aria-label="Add"
+																		component={App}
+																	>
+																		
+																		<EditIcon />
+																	</Button>
+																:
+																	''
+																
+																
 															)
 														})
 														
