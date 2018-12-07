3d249ae2287cab0ce71919242cdbcf6a4d10406b
Thuy Duong <duongdethuong28797@gmail.com>
2018-12-07 18:19:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from "./components/SideBar/SideBar";
 // import Home from "./components/Home/Home";
+import axios from 'axios';
+import Login from "./components/LoginNew/Login";
 import { 
 	BrowserRouter as Router, Route 
 } from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-		<Router>
-			<Sidebar />
-		</Router>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+		authentication: false
+		}
+	}
+	async componentDidMount() {
+		// var self = this
+		const response = await axios({
+			method: 'get',
+			url: 'http://localhost:5000/admin/hello',
+			withCredentials:true
+		})
+		if(response.data.status == "ok") {
+			this.setState({
+				authentication : true
+			})
+		}
+	}
+	render() {
+		if(this.state.authentication) {
+			return (
+				<Router>
+					<Sidebar />
+				</Router>
+			)
+			
+		} 
+		return (
+			<div>
+				<Login/>
+			</div>
+		)
+	  }
 }
 
 export default App;
