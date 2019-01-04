282bd0a87e1d0cc42744d233623135e347263c23
thuhien191197 <thuhien191197@gmail.com>
2019-01-04 17:22:22 +0700

    update

--- src/components/LoginNew/Login.js
+++ src/components/LoginNew/Login.js
@@ -6,10 +6,17 @@ export default class Login extends Component {
     super(props);
     this.state = {
       username: "",
-      password: ""
+	  password: "",
+	  
+	  isOpenUser: false,
+	  isOpenPass: false,
     }
     this.handleChangeUsername = this.handleChangeUsername.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
+	this.handleChangePassword = this.handleChangePassword.bind(this);
+	this.handleChangeOpenUser = this.handleChangeOpenUser.bind(this);
+	this.handleChangeCloseUser = this.handleChangeCloseUser.bind(this);
+	this.handleChangeOpenPass = this.handleChangeOpenPass.bind(this);
+	this.handleChangeClosePass = this.handleChangeClosePass.bind(this);
     // this.login = this.login.bind(this);
   }
   
@@ -19,9 +26,33 @@ export default class Login extends Component {
   }
   
   handleChangeUsername(event) {
-     this.setState({username: event.target.value});
-     console.log("change un")
+     this.setState({
+		username: event.target.value
+	});
+
   }
+  handleChangeOpenUser(){
+	this.setState({
+		isOpenUser: true
+	});
+  }
+  handleChangeCloseUser(){
+	this.setState({
+		isOpenUser: false
+	});
+  }
+  handleChangeOpenPass(){
+	this.setState({
+		isOpenPass: true
+	});
+  }
+  handleChangeClosePass(){
+	this.setState({
+		isOpenPass: false
+	});
+  }
+  
+
 
   // login() {
   //    var form = new FormData()
@@ -47,52 +78,41 @@ export default class Login extends Component {
   // }
 
   render() {
+		console.log("isOpenUser: " , this.state.isOpenUser)
     return (
-      <div>
-        <title />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="keywords" content="Existing Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
-        <link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
-        <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-        <link href="//fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet" />
-        <div className="w3layoutscontaineragileits">
+		<div className="form-container flip">
+			<form id="login" action="/api/login" method="POST" className="login-form">
+			<h3 className="title">Quản Lý Cơ Sở Vật Chât</h3>
+			<div className="form-group" id="username">
+				<input className="form-input"  name="username" value={this.state.username} onChange={this.handleChangeUsername} onFocus={this.handleChangeOpenUser} onBlur={this.handleChangeCloseUser} tooltip-class="username-tooltip" placeholder="Username" id="email"  required={true}></input>
+				{this.state.isOpenUser
+				?<span id="username-tool"className="tooltip username-tooltip">What's your username?</span>
+				:''
+				}
+				
+			</div>
+			<div className="form-group" id="password">
+				<input  type="password" name="password" value={this.state.password} onChange={this.handleChangePassword} onFocus={this.handleChangeOpenPass} onBlur={this.handleChangeClosePass} className="form-input" tooltip-class="password-tooltip" placeholder="Password" required={true}></input>
+				{this.state.isOpenPass
+				?<span className="tooltip password-tooltip">What's your password?</span>
+				:''
+				}
+			</div>
+			<div className="form-group">
+				<button type="submit" value="LOGIN" id="loginbtn" className="login-button">Login</button>
+				<input className="remember-checkbox" type="checkbox"></input>
+				<p className="remember-p">Remember me</p>
+			</div>
+			</form>
+			<div className="loading">
+			<div className="loading-spinner-large" ></div>
+			<div className="loading-spinner-small" ></div>
+			</div>
+			<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'></script>
+			<script src='https://code.jquery.com/jquery-2.1.4.min.js'></script>
 
-          <div className="title">
-            <h2>Login</h2>
-          </div>
-          <div className="component">
-            <form id="login" action="/api/login" method="POST">
-              <input type="text" name="username" placeholder="EMAIL" value={this.state.username} onChange={this.handleChangeUsername} required id="username" />
-              <input type="password" name="password" placeholder="PASSWORD"value={this.state.usernamePassword} onChange={this.handleChangePassword} required id="password" />
-              <ul className="agileinfotickwthree">
-                                                                                    <li>
-                  <input type="checkbox" id="brand1" defaultValue />
-                  <label htmlFor="brand1" className="remmeberMe"><span />Remember me</label>
-                  <a href="#">Forgot password?</a>
-                </li>
-              </ul>
-
-              <div className="aitssendbuttonw3ls">
-                <button type="submit" value="LOGIN" id="loginbtn" className="button_Login" >
-                  Login
-                </button>
-                <div className="clear" />
-              </div>
-            </form>
-
-            <div className="contentLogin">
-              <p>
-              </p><h6 className="content_Right">Lưu ý</h6>
-              <p>Trang này cho phép đăng nhập một lần đến nhiều hệ thống web ở trường Đại học Bách Khoa Đà Nẵng. Điều này có nghĩa là bạn chỉ đăng nhập một lần cho những hệ thống web đã đăng kí với hệ thống xác thực quản lý truy cập tập trung. <br />
-                Vì lí do bảo mật hãy Thoát khỏi trình duyệt web  khi bạn kết thúc việc truy cập các dịch vụ đòi hỏi xác thực ! </p> <br />
-              <h6 className="content_Right">Hỗ trợ kĩ thuật</h6>
-              <p>Email : duong@gmail.com | ĐT : 09095738936</p>
-              <p />
-            </div>
-          </div>
-        </div>
-      </div>
+		</div>
     );
   }                    
 }
